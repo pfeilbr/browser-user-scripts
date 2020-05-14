4b43d5f2c9db676e593428f61ff5a513abef761f
Brian Pfeil <brian.pfeil@gmail.com>
2020-05-14 11:52:59 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# browser-user-scripts
+
+* personal browser [user scripts](https://en.wikipedia.org/wiki/Userscript)
+* used via [Tampermonkey](https://www.tampermonkey.net/) for Chrome
---
+++ aws.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         AWS Content Focus
+// @namespace    http://aws.amazon.com/
+// @version      0.1
+// @description  remove extra content from AWS content
+// @author       Brian Pfeil
+// @match        https://aws.amazon.com/*
+// @grant        none
+// @updateURL   https://raw.githubusercontent.com/pfeilbr/browser-user-scripts/aws.js
+// @downloadURL https://raw.githubusercontent.com/pfeilbr/browser-user-scripts/aws.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  const focusPageContent = () => {
+    // remove fat header content
+    document.querySelector("#aws-page-header").remove();
+    document.querySelector("body > div:nth-child(1)").remove();
+    document
+      .querySelector("#aws-page-content > div.aws-blog-related-posts")
+      .remove();
+
+    // shrink top margin
+    document.querySelector("#aws-page-content").style.marginTop = "10px";
+
+    // remove right sidebar content
+    document.querySelector("#aws-page-content > div > div").remove();
+
+    // increase content width to fill page more
+    document.querySelector("#aws-page-content > div > main").style.width =
+      "80%";
+  };
+
+  document.addEventListener(
+    "keyup",
+    (e) => {
+      if (e.key === "f") {
+        console.log(
+          `user script: focusing page content.  triggered by pressing "${e.key}" key`
+        );
+        focusPageContent();
+      }
+    },
+    false
+  );
+})();
